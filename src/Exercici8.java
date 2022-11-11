dc0e2b6b4778d66c27361b6b724960b28e9f5851
Pau <pauramisnunez@gmail.com>
2022-11-11 16:53:13 +0100

    start8

---
+++ src/Exercici8.java
@@ -0,0 +1,6 @@
+public class Exercici8 {
+    public static void main(String[] args) {
+        //Escriu una funció que retorna una còpia d'un array bidimensional d'enters, que li passem
+        //com a paràmetre.
+    }
+}
