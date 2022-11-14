7540a8b0c5266afca92ed7f49b1d73ec2ed4eb54
Pau <pauramisnunez@gmail.com>
2022-11-14 16:12:25 +0100

    9

--- src/Exercici8.java
+++ src/Exercici8.java
@@ -2,5 +2,8 @@ public class Exercici8 {
     public static void main(String[] args) {
         //Escriu una funció que retorna una còpia d'un array bidimensional d'enters, que li passem
         //com a paràmetre.
+
+        int[][] ar = {{12, 5}, {2, 4}, {3, 6}};
+
     }
 }
---
+++ src/Exercici9.java
@@ -0,0 +1,11 @@
+public class Exercici9 {
+    public static void main(String[] args) {
+        //Què imprimeix el següent codi?
+        int[] a = { 1, 2, 3 };
+        int[] b = { 1, 2, 3 };
+        System.out.println(a == b);
+
+        //El codi es dedica només a dir si els dos arrays són el mateix, dona "false",
+        //perque encara que tenen el mateix contengut, són distints objectes
+    }
+}
