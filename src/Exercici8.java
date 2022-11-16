070964fc4b15ce2526519b4886b1e065df64a9d4
Your Name <you@example.com>
2022-11-16 15:28:47 +0100

    8

--- src/Exercici8.java
+++ src/Exercici8.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Exercici8 {
     public static void main(String[] args) {
         //Escriu una funció que retorna una còpia d'un array bidimensional d'enters, que li passem
@@ -5,5 +7,9 @@ public class Exercici8 {
 
         int[][] ar = {{12, 5}, {2, 4}, {3, 6}};
 
+        int[][] array2 = Arrays.copyOf(ar, ar.length);
+
+        //Usam deepToString pels arrays bidimensionals
+        System.out.println(Arrays.deepToString(array2));
     }
 }
