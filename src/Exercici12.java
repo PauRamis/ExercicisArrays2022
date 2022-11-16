0b142fc3e373521c515f441bce23ad06be91ebcd
Your Name <you@example.com>
2022-11-16 17:44:29 +0100

    12Start

--- src/Exercici12.java
+++ src/Exercici12.java
@@ -1,2 +1,21 @@
 public class Exercici12 {
+    public static void main(String[] args) {
+        //Donats dos vectors, escriu una funció que retorni el seu producte escalar i una altra funció
+        //que retorni el producte vectorial.
+
+        int[] a = {1, 2};
+        int[] b = {5, 3};
+        producteEscalar(a, b);
+        producteVectorial(a, b);
+    }
+
+    private static void producteEscalar(int[] a, int[] b) {
+        //v1 · v2 = (v1[0] * v2 [0]) + (v1[1] * v2[1])
+        System.out.println("Producte Escalar: " + ((a[0] * b[0]) + (a[1] * b[1])));
+    }
+
+    private static void producteVectorial(int[] a, int[] b) {
+        //
+    }
+
 }
